837cd2fee887b5c53e99c047d1f783ed0296a0ba
Darien Hernandez <darienhz89@gmail.com>
2021-01-18 20:08:12 -0800

    coded home.js

---
+++ app/javascript/components/Home/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+
+class Home extends Component {
+  constructor(){
+    this.state = {
+      course_modules: [
+        { id: 1, title: '1.Setting up a new Ruby on Rails app with React', description: 'lorem ipsum', active: false },
+        { id: 2, title: '2.Adding React to an existing rails app', description: 'lorem ipsum', active: false },
+        { id: 3, title: '3.Building a Hello World app', description: 'lorem ipsum', active: false },
+        { id: 3, title: '4.Adding React Router Dom to your app', description: 'lorem ipsum', active: false },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div>This is our home component</div>
+    )
+  }
+}
+
+export default Home
