0871a8fcfbb0eeecb16caa07ca9230d623288681
Darien Hernandez <darienhz89@gmail.com>
2021-01-19 14:11:49 -0800

    added the styling for cta button on items

--- app/javascript/components/Home/Table/Item.js
+++ app/javascript/components/Home/Table/Item.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react'
 import Thumbnail from './Thumbnail'
+import styled from 'styled-components'
 
+const Button = styled.a`
+  display: inline-block;
+  text-decoration: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 4px;
+  background: #fff;
+  color: #333 !important;
+  padding: 10px 20px;
+  font-size: 18px;
+  box-shadow: 0px 0px 0px 3px #473228,
+  -6px 6px #ef5f17,
+  -6px 6px 0px 3px #473228;
+`
 const Item = (props) => {
   return (
     <div className="row pt-4 pb-4">
@@ -16,7 +31,7 @@ const Item = (props) => {
                   <h4>{props.title}</h4>
                   <p>{props.description}</p>
                   <div className="cta-wrapper">
-                    <a className="btn cta-btn">Watch Video</a>
+                    <Button className="btn cta-btn">Watch Video</Button>
                   </div>
                 </div>
               </div>
