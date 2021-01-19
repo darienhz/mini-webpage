78e96defc00eb8c9c0b14217b059cc07804123fe
Darien Hernandez <darienhz89@gmail.com>
2021-01-18 21:09:43 -0800

    fixed the styling for my button

--- app/javascript/components/Home/Jumbotron.js
+++ app/javascript/components/Home/Jumbotron.js
@@ -24,6 +24,9 @@ const Button = styled.a`
   cursor: pointer;
   border-radius: 4px;
   background: #fff;
+  color: #333 !important;
+  padding: 10px 20px;
+  font-size: 18px;
   box-shadow: 0px 0px 0px 3px #473228,
   -6px 6px #ef5f17,
   -6px 6px 0px 3px #473228;
